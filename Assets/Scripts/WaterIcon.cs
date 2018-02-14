bd4a9a249939ffdede5bd101a44b11e933d59839
Unknown <wrightCoder@gmail.com>
2018-02-14 13:47:24 -0500

    :sparkles: Water drop ui

---
+++ Assets/Scripts/WaterIcon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterIcon : MonoBehaviour {
+
+	public Transform parentWater;
+	public Transform EmptyTarget;
+	public Transform FullTarget;
+
+	public GameObject waterSprite;
+	public GameObject dirtyWaterSprite;
+
+	public bool isTouching = false;
+	public float waterRatio = 0;
+	public bool waterDirty = false;
+	float waterlevel = 0f;
+	Vector3 targetScale;
+	// Use this for initialization
+	void Start () {
+		targetScale = transform.localScale;
+		waterlevel = waterRatio;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		waterSprite.SetActive(!waterDirty);
+		dirtyWaterSprite.SetActive(waterDirty);
+
+		Vector3 target = Vector3.Lerp(EmptyTarget.position,FullTarget.position, waterRatio);
+		parentWater.transform.position = Vector3.Slerp(parentWater.position,target,Time.deltaTime * 2f);
+
+		Vector3 sizeTarget = Vector3.zero;
+		if(isTouching){
+			sizeTarget = targetScale;
+		}
+		transform.localScale = Vector3.Slerp(transform.localScale, sizeTarget, Time.deltaTime * 5f);
+
+	
+	}
+}
