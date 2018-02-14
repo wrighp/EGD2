2b351311feba8800fb5a79a43fa4dce4880156d1
Unknown <wrightCoder@gmail.com>
2018-02-14 00:47:28 -0500

    🎵 Sound files and Sound Manager scripts

---
+++ Assets/Scripts/SoundDestroyer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class SoundDestroyer : MonoBehaviour
+{
+	bool activated = false;
+	AudioSource source;
+
+	void Start(){
+		source = GetComponent<AudioSource>();
+	}
+
+	void Update(){
+		if(source.isPlaying){
+			activated = true;
+		}
+		else if (activated){
+			Destroy(gameObject);
+		}
+	}
+}
+
